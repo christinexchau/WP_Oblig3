7755423600ad761889690dcf26e31ee676f75f48
christinexchau <143170525+christinexchau@users.noreply.github.com>
2024-05-02 17:20:30 +0200

    update on html, javascript and Ticket.java

--- src/main/java/oslometwebpro/wp_oblig3/Ticket.java
+++ src/main/java/oslometwebpro/wp_oblig3/Ticket.java
@@ -2,5 +2,69 @@ package oslometwebpro.wp_oblig3;
 
 public class Ticket {
     private String movie;
-    private int
+    private int number;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int phonenumber;
+
+    public Ticket(String movie, int number, String firstName, String lastName, String email, int phonenumber) {
+        this.movie = movie;
+        this.number = number;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phonenumber = phonenumber;
+    }
+
+    public Ticket (){
+    }
+
+    public String getMovie() {
+        return movie;
+    }
+
+    public void setMovie(String movie) {
+        this.movie = movie;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(int phonenumber) {
+        this.phonenumber = phonenumber;
+    }
 }
---
+++ src/main/resources/static/index.js
@@ -0,0 +1,106 @@
+
+//Funksjon som registrerer billetten
+function regBillett () {
+    const enBillett = {
+        film: $('#film').val(),
+        antall: $('#antall').val(),
+        fornavn: $('#fornavn').val(),
+        etternavn: $('#etternavn').val(),
+        epost: $('#epost').val(),
+        telefonnr: $('#telefonnr').val()
+    };
+
+    //Legger til valideringsinputer
+    if(Object.values(enBillett).includes("") || enBillett.film === "Velg film her") {
+        if (enBillett.antall === "") {
+            $("#antallFeil").html("Du mangler å skrive inn antall");
+        } else {
+            $("#antallFeil").html("");
+        }
+        if (enBillett.fornavn === "") {
+            $("#fornavnFeil").html("Du mangler fornavn")
+        } else {
+            $("#fornavnFeil").html("");
+        }
+        if (enBillett.etternavn === "") {
+            $("#etternavnFeil").html("Du mangler etternavn")
+        } else {
+            $("#etternavnFeil").html("");
+        }
+        if (enBillett.epost === "" || !gyldigEpost(enBillett.epost)) {
+            $("#epostFeil").html("Du må skrive inn en gyldig epost")
+        } else {
+            $("#epostFeil").html("");
+        }
+        if (enBillett.telefonnr === "" || enBillett.telefonnr.length !== 8) {
+            $("#telefonnrFeil").html("Du må skrive inn et gyldig telefonnummer")
+        } else {
+            $("#telefonnrFeil").html("");
+        }
+    } else {
+
+        //Lagrer inputene inni server
+        $.post("/lagre", enBillett, function () {
+            hentAlle();
+        });
+        //Tømmer inputene
+        $("#film").val("");
+        $("#antall").val("");
+        $("#fornavn").val("");
+        $("#etternavn").val("");
+        $("#epost").val("");
+        $('#telefonnre').val("");
+    }
+}
+function hentAlle() {
+    $.get("/hentAlle", function (billetter) {
+        formaterData(billetter);
+    });
+}
+
+//Viser fram inputene i en tabell
+function formaterData(billetter) {
+    let ut = "<table class='table table-striped'><tr><th>Film</th><th>Antall</th><th>Fornavn</th>" +
+        "<th>Etternavn</th><th>Epost</th><th>Telefonnummer</th></tr>";
+    for (const billett of billetter) {
+        ut += "<tr><td>" + billett.film
+            + "</td><td>" + billett.antall
+            + "</td><td>" + billett.fornavn
+            + "</td><td>" + billett.etternavn
+            + "</td><td>" + billett.epost
+            + "</td><td>" + billett.telefonnr
+            + "</td></tr>";
+    }
+    ut += "</table>";
+    $("#billettene").html(ut);
+}
+
+//Funksjon som sletter alle billetter
+function slettAlle() {
+    $.get("/slettAlle", function () {
+        hentAlle();
+    });
+}
+
+//Validering for gyldig epost
+function gyldigEpost(epost) {
+    const epostRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,}$/;
+    return epostRegex.test(epost);
+}
+
+/*
+Validering for gyldig telefonnr
+
+function gyldigTelefon(telefonnr) {
+    const telefonRegex = /^(4[0-9]{7}|9[0-9]{7})$/;
+    return telefonRegex.test(telefonnr);
+}
+
+Denne funksjonen funker merkelig nok ikke, og det samme gjelder if-setningen:
+ enBillett.telefonnr.length != 8
+ !gyldigTelefon(enBillett.telefonnr)
+
+Jeg har også fått hjelp hos Orakel, og de fikk det heller ikke til.
+Ellers funker if-setningen når inputfelten er tom.
+
+ */
