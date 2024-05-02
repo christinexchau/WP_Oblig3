f70391c6615e4564bc7a53128e0e2ef5aaf6dffa
christinexchau <143170525+christinexchau@users.noreply.github.com>
2024-05-02 17:29:20 +0200

    update on repository and controller

--- src/main/java/oslometwebpro/wp_oblig3/TicketController.java
+++ src/main/java/oslometwebpro/wp_oblig3/TicketController.java
@@ -1,4 +1,34 @@
 package oslometwebpro.wp_oblig3;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
 public class TicketController {
+
+    @Autowired
+    private TicketRepository rep;
+
+    //Lagrer inputene på server
+    @PostMapping("/save")
+    public void saveTicket(Ticket ticket) {
+        rep.saveTicket(ticket);
+    }
+
+    //Henter billettene fra server
+    @GetMapping("/hentAlle")
+    public List<Ticket> henteAlle(){
+        return rep.hentAlleBilletter();
+    }
+
+    //Sletter billettene fra server
+    @GetMapping("/deleteAll")
+    public void deleteAll() {
+        rep.deleteAllTickets();
+    }
 }
--- src/main/java/oslometwebpro/wp_oblig3/TicketRepository.java
+++ src/main/java/oslometwebpro/wp_oblig3/TicketRepository.java
@@ -1,4 +1,39 @@
 package oslometwebpro.wp_oblig3;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class TicketRepository {
+
+    @Autowired
+    private JdbcTemplate db;
+
+    // Lagrer inputverdinene inni tabellen
+    public void saveTicket (Ticket ticket) {
+        String sql = "INSERT INTO Ticket (movie, number, firstname, lastname, email, phonenumber) VALUES (?,?,?,?,?,?)";
+        db.update(sql, ticket.getMovie(),
+                ticket.getNumber(),
+                ticket.getFirstName(),
+                ticket.getLastName(),
+                ticket.getEmail(),
+                ticket.getPhonenumber());
+    }
+
+    // Legger til en SQL-kommando der brukeren henter alle billetter
+    public List<Ticket> hentAlleBilletter() {
+        String sql = "SELECT * FROM Ticket";
+        List<Ticket> allTickets = db.query(sql, new BeanPropertyRowMapper(Ticket.class));
+        return allTickets;
+    }
+
+    // Legger til en SQL-kommando der brukeren sletter alle billetter
+    public void deleteAllTickets() {
+        String sql = "DELETE FROM Ticket";
+        db.update(sql);
+    }
 }
